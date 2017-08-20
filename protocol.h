19c8824bd4124109671f85946de330cb9a054bc8
MohsenMortezaie <mohsenmortezaie1@gmail.com>
2017-08-20 05:52:36 -0500

    Update protocol.h

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 18333 : 18666;
+    return testnet ? 11131 : 11130;
 }
 
 //
